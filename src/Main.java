b0894712ca9cc1e660067cc4a853c9f1d0f34e18
Kevin Kulman <kdkulman@gmail.com>
2022-08-12 23:27:40 -0700

    Corrected algorithm

--- src/Main.java
+++ src/Main.java
@@ -9,31 +9,30 @@ public class Main {
             //No duplicate elements
 
         //Feel free to alter the array while adhering to constraints
-        int[] A = {3, 7, 20, 43, 50};
-        int[] B = {1, 14, 16, 37, 42};
+        int[] A = {1, 2, 3, 4};
+        int[] B = {5, 6, 7, 8};
         int answer = findUpperMedianOfTwoSortedLists(A, B);
         System.out.println("Upper median of two sorted lists: " + answer);
     }
 
     private static int findUpperMedianOfTwoSortedLists(int[] A, int[] B) {
         int n = A.length;
-        int upperMedian = (int) (Math.floor(n / 2.0));
+        int upperMedian = (int) (Math.ceil(n / 2.0));
         if (n == 1) return Math.max(A[0], B[0]);
         if (A[upperMedian] > B[upperMedian]){
-            A = cutArrayInHalf(0, upperMedian, A);
-            B = cutArrayInHalf(upperMedian, n - 1, B);
+            A = cutArrayInHalf(0, upperMedian - 1, A);
+            B = cutArrayInHalf(n - upperMedian, n - 1, B);
         } else {
-            A = cutArrayInHalf(upperMedian, n - 1, A);
-            B = cutArrayInHalf(0, upperMedian, B);
+            A = cutArrayInHalf(n - upperMedian, n - 1, A);
+            B = cutArrayInHalf(0, upperMedian - 1, B);
         }
         return findUpperMedianOfTwoSortedLists(A, B);
     }
 
     private static int[] cutArrayInHalf(final int start, final int end, int[] array){
-        int[] arrayToReturn = new int[end - start + 1];
-        for (int i = 0; i < arrayToReturn.length; i++){
-            arrayToReturn[i] = array[start+i];
-        }
+        int length = end - start + 1;
+        int[] arrayToReturn = new int[length];
+        for (int i = 0; i < length; i++) arrayToReturn[i] = array[start+i];
         return arrayToReturn;
     }
 }
